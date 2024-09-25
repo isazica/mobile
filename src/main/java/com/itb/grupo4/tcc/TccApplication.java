87b36d531055bbc88d6de6f3b389a740895d971e
alce606 <rm90619@estudante.fieb.edu.br>
2024-09-25 09:55:30 -0300

    teste

--- src/main/java/com/itb/grupo4/tcc/TccApplication.java
+++ src/main/java/com/itb/grupo4/tcc/TccApplication.java
@@ -9,7 +9,7 @@ public class TccApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(TccApplication.class, args);
-		System.out.println("Ola mundo");
+		System.out.println("Ola ");
 	}
 
 }
