3b5dae030a408c96e4b155e8c4a9f8260fd9c734
alce606 <159257809+alce606@users.noreply.github.com>
2025-05-27 04:54:07 -0300

    Delete src directory

--- src/main/java/com/itb/grupo4/tcc/exceptions/BadRequest.java
+++
@@ -1,8 +0,0 @@
-package com.itb.grupo4.tcc.exceptions;
-
-public class BadRequest extends RuntimeException {
-
-    public BadRequest(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/itb/grupo4/tcc/repository/CadastroRepository.java
+++
@@ -1,9 +0,0 @@
-package com.itb.grupo4.tcc.repository;
-
-import com.itb.grupo4.tcc.model.Cadastro;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CadastroRepository extends JpaRepository<Cadastro, Long> {
-}
